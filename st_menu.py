54f94ba8f4b944b92fbf9b8675c30872e88dcea2
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-03-24 21:49:05 -0700

    Update and rename menu.py to st_menu.py
    pendientes
    - crear objetos tipo usuario de prueba y anexarlos a la lista
    -encriptacion
    -pagina de inico y su respectivo cagadero
                Sesion iniciada
        Ver perfil automaticamente (Bio, number de seguidores, n. post)
        Ver feed
            Editar perfil (nombre, bio, contraseña)
            Crear tweet (excepción limite 100 char)
            Eliminar tweet 
            Follow otras cuentas (búsqueda nombre o correo)
            DM (buzon) 
            Cerrar sesion
                Menu principal

--- st_menu.py
+++ st_menu.py
@@ -1,11 +1,15 @@
 from user import User
 
-users = {}
+userPasswords = {}
+"""Diccionario de usuarios/contraseñas"""
 all_users = []
+"""Lista de usuarios"""
 logged_in = 0
+"""logged_in = 1 cuando la sesion ya esta iniciada"""
 
 
 def displayMenu():
+    """ejecuta el menu principal"""
     status = input("""Welcome to Twitter\t
 Are you already registered? Press "y" for Yes, "n" for No
     """)
@@ -16,34 +20,40 @@ Are you already registered? Press "y" for Yes, "n" for No
 
 
 def register():
+    """registra usuario nuevo"""
     name = input("name: ")
     username = input("username")
     password = input("password")
     email = input("email")
     date_of_birth = input("Dob ")
-    id = input("Id")
     bio = input("bio")
     interests = input("intereses")
-    if username in users:
+    if username in userPasswords:
         print("\nLogin name already exist!\n")
     else:
-        u = User(name, username, password, email, date_of_birth, id, bio, interests)
-        users[username] = password
-        all_users.append
+        """aqui se crea el nuevo objeto de clase User"""
+        u = User(name, username, password, email, date_of_birth, bio, interests)
+        userPasswords[username] = password
+        all_users.append(u)
         print ("Success!!")
 
 
 def sign_in():
+    """inicia sesion"""
     login = input("Enter login name: ")
     passw = input("Enter password: ")
 
-    if login in users and users[login] == passw:
+    if login in userPasswords and userPasswords[login] == passw:
         print("\nLogin successful!\n")
-        return logged_in += 1
+        global logged_in
+        logged_in += 1
     else:
         print("\nUser doesn't exist or wrong password!, try again\n")
+
 
 while logged_in == 0 :
     displayMenu()
 else:
-        print ("Sesion Inciada, Bienvenido")
+        print ("You are now logged in, welcome")
+        print (userPasswords)
+        print (all_users)
