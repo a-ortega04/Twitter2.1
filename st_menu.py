a1b11bb233e991be50534f7ccae29bb92154c47d
Juan1541 <73973316+Juan1541@users.noreply.github.com>
2021-03-25 19:10:38 -0700

    Update st_menu.py

--- st_menu.py
+++ st_menu.py
@@ -12,7 +12,7 @@ logged_in = 0
 
 def displayMenu():
     """ejecuta el menu principal"""
-    status = input("""Welcome to Twitter\t
+    status = input("""Welcome to Twitter \U0001F60A\t
 Are you already registered? Press "y" for Yes, "n" for No
     """)
     if status == "y":
@@ -23,40 +23,40 @@ Are you already registered? Press "y" for Yes, "n" for No
 
 def register():
     """registra usuario nuevo"""
-    name = input("name: ")
-    username = input("username")
-    password = input("password")
-    email = input("email")
-    date_of_birth = input("Dob ")
-    bio = input("bio")
-    interests = input("intereses")
+    name = input("Please put your first and last name: ")
+    username = input("Please type a username: ")
+    password = input("Please type a password: ")
+    email = input("Please insert your email: ")
+    date_of_birth = input("Please type your Dob:  ")
+    bio = input("Please create a bio: ")
+    interests = input("Please list some of your intereses: ")
     if username in userPasswords:
-        print("\nLogin name already exist!\n")
+        print("\nLogin name already exist! \U0001F62C \n")
     else:
         """aqui se crea el nuevo objeto de clase User"""
         u = User(name, username, password, email, date_of_birth, bio, interests)
         userPasswords[username] = password
         all_users.append(u)
-        print ("Success!!")
+        print ("Success!!\U0001F60D")
 
 
 def sign_in():
     """inicia sesion"""
-    login = input("Enter login name: ")
-    passw = input("Enter password: ")
+    login = input("Enter your login name: ")
+    passw = input("Enter your password: ")
 
     if login in userPasswords and userPasswords[login] == passw:
-        print("\nLogin successful!\n")
+        print("\nLogin successful! \U0001F618 \n")
         global logged_in
         logged_in += 1
     else:
-        print("\nUser doesn't exist or wrong password!, try again\n")
+        print("\nUser doesn't exist or wrong password!, try again! \U0001F914\n")
 
 
 while logged_in == 0:
     displayMenu()
 else:
-        print ("You are now logged in, welcome")
+        print ("You are now logged in, welcome \U0001F609"	)
         print (userPasswords)
         print (all_users)
         #mainMenu()
