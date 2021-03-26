5c3435d2b7c28a337f8de4046a0419a8fc83854e
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-03-25 18:55:39 -0700

    Update user.py

--- user.py
+++ user.py
@@ -16,6 +16,10 @@ class User:
         self.date_of_birth = date_of_birth
         self.bio = bio
         self.interests = interests  # hacer lista
+       
+juan = User("Juan of the Tower", "juanito", "123", "j@gmail.com", "2-2-1913", "i like baseball, and fortnite", "baseball, videogames, hot chicks")
+ivan = User("Ivan Sanchez", "chupapi", "123", "chupapi@gmail.com", "4-20-69", "He/him, ICC, Proud husband and father of two ", "football, videogames and hot chicks")
+jerry = User("Gerardo del Rincon", "jerryman5000", "123", "dudasconjerry@gmail.com", "2-05-46", "Proud husband and father of one, fuck facebook", "Programming, hot chicks")
 
 
 
