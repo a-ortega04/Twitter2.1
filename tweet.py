8a07134a28361cc1c5950c7a33c8451f62e41d15
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-04-06 13:53:01 -0700

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,30 +1,29 @@
-from tweet import Tweet, Feed
+import datetime
+latest_tweet = 0
+
+class Tweet:
+
+    def __init__(self, user=[], content=0, likes=0):
+        self.user = user
+        self.content = content
+        self.likes = likes
+        global latest_tweet
+        latest_tweet  +=1
+        self.tid = latest_tweet
+        self.creation_date = datetime.date.today()
+
+    def match(self, filter):
+         """search tool"""
+         return filter in self.content
 
 
 
-t = Feed()
-t.new_tweet("juan", "juanbito me la pela", 3)
-t.new_tweet("carlos", "ivan es un dios", 5)
-t.new_tweet("dsa", "ivanasdos", 5)
+class Feed:
+    def __init__(self):
+        self.tweets = []
 
+    def new_tweet(self, user, content, likes):
+        self.tweets.append(Tweet(user, content, likes))
 
-def printWall():
-        print(t.tweets[0].user)
-        print(t.tweets[0].creation_date)
-        print(t.tweets[0].content)
-        print(t.tweets[0].likes)
-        print(t.tweets[1].user)
-        print(t.tweets[1].creation_date)
-        print(t.tweets[1].content)
-        print(t.tweets[1].likes)
-        print(t.tweets[2].user)
-        print(t.tweets[2].creation_date)
-        print(t.tweets[2].content)
-        print(t.tweets[2].likes)
-
-printWall()
-
-  def printTweet(self):
-        print("""{}, {} , 
-{}
-Likes: {}""". format( self.user, self.creation_date, self.content
+    def search(self, filter):
+        return [tweet for tweet in self.tweets if tweet.match(filter)]
