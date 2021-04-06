b23f7003361785b70f18785c48b2a56d03c91aee
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-04-05 20:51:32 -0700

    Update user.py

--- user.py
+++ user.py
@@ -1,25 +1,42 @@
+import hashlib
+
 class User:
 
 
-    def __init__(self, name=0, username=0, password=0, email=0, date_of_birth=0, bio=0, interests=0):
+    def __init__(self, name, username, password, email, date_of_birth, bio, interests, followers=0):
         """crea el nuevo objeto cuenta, NO se encuentra loggeado por default
         :param name: nombre del usuario
         :param username: username de usuario
         :param password: contraseña previemente encriptada
         :param email: email de registro
-        :param id: numero de identificacion unico
         """
         self.name = name
         self.username = username
-        self.password = password
+        self.password = self._encrypt_pw(password)
         self.email = email
         self.date_of_birth = date_of_birth
         self.bio = bio
         self.interests = interests  # hacer lista
-       
-juan = User("Juan of the Tower", "juanito", "123", "j@gmail.com", "2-2-1913", "i like baseball, and fortnite", "baseball, videogames, hot chicks")
-ivan = User("Ivan Sanchez", "chupapi", "123", "chupapi@gmail.com", "4-20-69", "He/him, ICC, Proud husband and father of two ", "football, videogames and hot chicks")
-jerry = User("Gerardo del Rincon", "jerryman5000", "123", "dudasconjerry@gmail.com", "2-05-46", "Proud husband and father of one, fuck facebook", "Programming, hot chicks")
+        self.followers = followers
+
+    def _encrypt_pw(self, password):
+        hash_string = self.username + password
+        hash_string = hash_string.encode("utf8")
+        return hashlib.sha256(hash_string).hexdigest()
+
+    def check_password(self, password):
+        """Return True if the password is valid for this
+        user, false otherwise."""
+        encrypted = self._encrypt_pw(password)
+        return encrypted == self.password
+
+    def printProfile(self):
+        print("""{}, {} 
+{}
+Followers: {}""". format( self.username, self.name, self.bio, self.followers))
 
 
+juanito = User("Juan of the Tower", "juanito", "123", "j@gmail.com", "2-2-1913", "i like baseball, and fortnite", "baseball, videogames, hot chicks")
+chupapi = User("Ivan Sanchez", "Chupapi", "123", "chupapi@gmail.com", "4-20-69", "He/him, ICC, Proud husband and father of two ", "football, videogames and hot chicks")
+jerryman5000 = User("Gerardo del Rincon", "jerryman5000", "123", "dudasconjerry@gmail.com", "2-05-46", "Proud husband and father of one, fuck facebook", "Programming, hot chicks")
 
