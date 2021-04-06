7a53c53d04b872757d787661197ce3b4bca0d899
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-04-06 13:26:31 -0700

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,22 +1,30 @@
-import datetime
-latest_tweet = 0
+from tweet import Tweet, Feed
 
-class Tweet:
-    def __init__(self, user, content, likes=0):
-        self.user = user
-        self.content = content
-        self.likes = likes
-        global latest_tweet
-        latest_tweet  +=1
-        self.tid = latest_tweet
-        self.creation_date = datetime.date.today()
 
-    # def match(self, filter):
-    #     """search tool"""
-    #     return filter in self.content
 
-    def printTweet(self):
+t = Feed()
+t.new_tweet("juan", "juanbito me la pela", 3)
+t.new_tweet("carlos", "ivan es un dios", 5)
+t.new_tweet("dsa", "ivanasdos", 5)
+
+
+def printWall():
+        print(t.tweets[0].user)
+        print(t.tweets[0].creation_date)
+        print(t.tweets[0].content)
+        print(t.tweets[0].likes)
+        print(t.tweets[1].user)
+        print(t.tweets[1].creation_date)
+        print(t.tweets[1].content)
+        print(t.tweets[1].likes)
+        print(t.tweets[2].user)
+        print(t.tweets[2].creation_date)
+        print(t.tweets[2].content)
+        print(t.tweets[2].likes)
+
+printWall()
+
+  def printTweet(self):
         print("""{}, {} , 
 {}
-Likes: {}""". format( self.user, self.creation_date, self.content, self.likes))
-
+Likes: {}""". format( self.user, self.creation_date, self.content
