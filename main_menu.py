a658cdefd3578dfb52bca2db8b6203190e5262cf
CRincon115 <73974058+CRincon115@users.noreply.github.com>
2021-04-06 16:50:46 -0500

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -1,15 +1,24 @@
-from tweet import Tweet, Feed
+from user import User, juanito, chupapi, jerryman5000
+
+userPasswords = {'juanito': '123', 'chupapi': '123', 'jerryman5000': '123'}
+"""Diccionario de usuarios/contraseñas"""
+all_users = []
+"""Lista de usuarios"""
+all_users.extend([juanito, chupapi, jerryman5000])
+
+user_logged_in = []
+"""usurio con sesion iniciada"""
 
 def options():
-    status = input(""" Choose the following options: 
-    1. Edit Profile(e.j Username, bio, passw, int, etc): "1"
-    2. Create a new Tweet: "2"
+    status = input(""" Choose the following options:
+    1. Edit Profile(e.j Name, bio, passw , etc): "1"
+    2. Create a new Tweet(Max of 100 characters): "2"
     3. Delete Tweet: "3"
     4. Follow other accounts(from name or email): "4"
     5. Close session: "5"
         """)
     if status == "1":
-        edit_Profile()
+        edit_Account()
     if status == "2":
         new_Tweet()
     if status == "3":
@@ -19,40 +28,44 @@ def options():
     if status == "5":
         close_ses()
 
-def edit_Profile():
-    print("your profile ")
+def edit_Account():
+    changes = (input("""What do you wanna change?
+    1. Change the username.
+    2. Change bio.
+    3. Change password.
+    4. Change interests.
+    5. Exit
+        """))
+    if changes == "1":
+        User.username = input("Please enter your new username.")
+        print("Your username has been successfully updated. ")
+        edit_Account()
+    elif changes == "2":
+        User.bio = input("Please add your new bio.")
+        print("Your bio has been successfully updated. ")
+        edit_Account()
+    elif changes == "3":
+        User.password = input("Please enter your new password:")
+        print("Your password has been succesfully updated.")
+        edit_Account()
+    elif changes == "4":
+        User.interests = input("Please enter your new interests.")
+        print("Your interests had been successfully updated. ")
+        edit_Account()
+    elif changes == "5":
+        options()
+    else:
+        print("Invalid option, please try again.")
+        edit_Account()
 
 def delete_Tweet():
     print("your tweet has been deleted")
 
 def new_Tweet():
-    print("Write Your Tweet: ")
-    user = User.username
-    t =
     print("you created a tweet")
 
 def follow_Acts():
-    print("You followed ")
+    print("You followed")
 
 def close_ses():
     pass
-
-def printWall():
-    print(t.tweets[0].user)
-    print(t.tweets[0].creation_date)
-    print(t.tweets[0].content)
-    print(t.tweets[0].likes)
-    print(t.tweets[1].user)
-    print(t.tweets[1].creation_date)
-    print(t.tweets[1].content)
-    print(t.tweets[1].likes)
-    print(t.tweets[2].user)
-    print(t.tweets[2].creation_date)
-    print(t.tweets[2].content)
-    print(t.tweets[2].likes)
-
-t = Feed()
-t.new_tweet("", "", 1)
-t.new_tweet("", "", 1)
-t.new_tweet("", "", 1)
-
