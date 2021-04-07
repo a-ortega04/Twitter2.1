77f34f4419167368683a2e5d8683d451691505b1
a-ortega04 <72634896+a-ortega04@users.noreply.github.com>
2021-04-06 19:44:43 -0700

    Update tweet.py

--- tweet.py
+++ tweet.py
@@ -1,6 +1,6 @@
 import datetime
-from vars import user_logged_in
 latest_tweet = 0
+
 
 class Tweet:
 
@@ -9,7 +9,7 @@ class Tweet:
         self.content = content
         self.likes = likes
         global latest_tweet
-        latest_tweet  +=1
+        latest_tweet += 1
         self.tid = latest_tweet
         self.creation_date = datetime.date.today()
 
@@ -17,21 +17,14 @@ class Tweet:
          """search tool"""
          return filter in self.content
 
-def new_Tweet():
-    user = user_logged_in
-    content = input ("Write Your Tweet: ")
-    t = Feed(user, content, 0)
-    Feed.tweets.insert(0,t)
-    print("you created a tweet")
 
-
-class Feed(Tweet):
-    def __init__(self, user=[], content=[], likes=0):
-        super().__init__(user=[], content=[], likes=0)
+class Feed:
+    def __init__(self):
         self.tweets = []
 
-    def new_tweet(self, user, content, likes):
+    def new_tweet(self, user, content, likes=0):
         self.tweets.append(Tweet(user, content, likes))
 
     def search(self, filter):
         return [tweet for tweet in self.tweets if tweet.match(filter)]
+
